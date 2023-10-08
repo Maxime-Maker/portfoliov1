8e3d534a6acf9372307465f72b724587e8c7e7d5
#Maxime <maxime.marie@gmx.com>
2023-10-08 13:31:06 +0200

    edit css

---
+++ src/pages/LoadingPage.jsx
@@ -0,0 +1,10 @@
+const LoadingPage = () => {
+  return (
+    <div className="loading">
+      <h1>Chargement en cours...</h1>
+      {/* Vous pouvez personnaliser le contenu de la page de chargement ici */}
+    </div>
+  );
+};
+
+export default LoadingPage;
